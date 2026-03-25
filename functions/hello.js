25e63f08ce26b5ee709a88162559359458a03c75
Tommi <tommi.karsi@gmail.com>
2026-03-25 04:13:32 +0200

    image onclick

--- functions/hello.js
+++
@@ -1,8 +0,0 @@
-export async function onRequest(context) {
-    return new Response(
-        JSON.stringify({ message: "Hello!" }),
-        {
-            headers: { "Content-Type": "application/json" }
-        }
-    );
-}
